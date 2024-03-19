25ca2c05476c2bb24d8436dc87d057a0bd0a5e25
Usine4.0 Houda <Usine@U4010>
2024-03-19 13:14:06 +0000

    second pushing

--- reproTracker_app/views.py
+++ reproTracker_app/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def login(request):
-    return render(request, 'login.html')
-
+    if request.method == 'POST':
+        print("Hiiiiii")
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username == "Houda" and password == "Houda":
+            return redirect('index')
+        else:
+            error_message = "Nom d'utilisateur ou mot de passe incorrect."
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+    
 def index(request):
     return render(request, 'index.html')
 
