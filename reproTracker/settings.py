a29296a3ffc3eaeba676d410db85203b68c5054c
ardouni <dohaardouni@gmail.com>
2024-04-15 12:36:44 +0000

    database config

--- reproTracker/settings.py
+++ reproTracker/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'reproTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'registre',
+        'USER': 'root',
+        'PASSWORD': 'Ardouni.123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
